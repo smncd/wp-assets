b818d86b167c30e8b36b25e9c96554e870fbda45
Simon <contact@smn.codes>
2022-08-18 23:14:14 +0200

    Start WPAssets class

---
+++ WPAssets.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ *  WPAssets
+ *  Copyright (C) 2022  Simon Lagerlöf
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+namespace WPAssets;
+
+/**
+ * WP Assets.
+ * 
+ * Class for managing WordPress theme or plugin assets.
+ *
+ * @package WPAssets
+ * @author Simon Lagerlöf <contact@smn.codes>
+ * @link https://gitlab.com/smncd/wp-assets
+ * @license GNU Public License v3 or later http://www.gnu.org/licenses/gpl-3.0.txt
+ * @copyright 2022  Simon Lagerlöf
+ * @version 0.1.0
+ */
+class WPAssets {
+
+  /**
+   * Handle prefix.
+   * 
+   * @var string
+   */
+  private static string $handle_prefix = 'wpassets';
+}
