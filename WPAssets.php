d8dcccc848987cbf8ad6474e35768c5bc37f0ba1
Simon <contact@smn.codes>
2023-10-10 17:32:47 +0200

    Add editorconfig and format

--- WPAssets.php
+++ WPAssets.php
@@ -20,7 +20,7 @@ namespace WPAssets;
 
 /**
  * WP Assets.
- * 
+ *
  * Class for managing WordPress theme or plugin assets.
  *
  * @package WPAssets
@@ -32,225 +32,231 @@ namespace WPAssets;
  */
 class WPAssets {
 
-  /**
-   * Handle prefix.
-   * 
-   * @var string
-   */
-  protected static string $handle_prefix = 'wpassets';
+    /**
+     * Handle prefix.
+     *
+     * @var string
+     */
+    protected static string $handle_prefix = 'wpassets';
 
-  /**
-   * Register stylesheet.
-   * 
-   * @param string $handle
-   * @param string $src
-   * @param array|null $dependencies
-   * @param string|null $version
-   * @param string|null $media
-   * 
-   * @return void
-   * 
-   * @see https://developer.wordpress.org/reference/functions/wp_register_style/
-   * @since 1.0.0
-   */
-  public static function register_style(  
-    string $handle, 
-    string $src, 
-    array $dependencies = [], 
-    string $version = null, 
-    string $media = 'all' 
-  ):void {
-    $dependencies = self::dependencies( $src, $dependencies );
-    $version      = self::version( $src, $version );
+    /**
+     * Register stylesheet.
+     *
+     * @param string $handle
+     * @param string $src
+     * @param array|null $dependencies
+     * @param string|null $version
+     * @param string|null $media
+     *
+     * @return void
+     *
+     * @see https://developer.wordpress.org/reference/functions/wp_register_style/
+     * @since 1.0.0
+     */
+    public static function register_style(
+        string $handle,
+        string $src,
+        array $dependencies = [],
+        string $version = null,
+        string $media = 'all'
+    ): void {
+        $dependencies = self::dependencies( $src, $dependencies );
+        $version      = self::version( $src, $version );
 
-    wp_register_style( $handle, $src, $dependencies, $version, $media );
-  }
+        wp_register_style( $handle, $src, $dependencies, $version, $media );
+    }
 
-  /**
-   * Register script.
-   * 
-   * @param string $handle
-   * @param string $src
-   * @param array|null $dependencies
-   * @param string|null $version
-   * @param bool $in_footer
-   * 
-   * @return void
-   * 
-   * @see https://developer.wordpress.org/reference/functions/wp_register_script/
-   * @since 1.0.0
-   */
-  public static function register_script(  
-    string $handle, 
-    string $src, 
-    array|null $dependencies = [], 
-    string $version = null, 
-    bool $in_footer = true
-  ):void {
-    $dependencies = self::dependencies( $src, $dependencies );
-    $version      = self::version( $src, $version );
+    /**
+     * Register script.
+     *
+     * @param string $handle
+     * @param string $src
+     * @param array|null $dependencies
+     * @param string|null $version
+     * @param bool $in_footer
+     *
+     * @return void
+     *
+     * @see https://developer.wordpress.org/reference/functions/wp_register_script/
+     * @since 1.0.0
+     */
+    public static function register_script(
+        string $handle,
+        string $src,
+        array|null $dependencies = [],
+        string $version = null,
+        bool $in_footer = true
+    ):  void {
+        $dependencies = self::dependencies( $src, $dependencies );
+        $version      = self::version( $src, $version );
 
 		wp_register_script( $handle, $src, $dependencies, $version, $in_footer );
-  }
-
-  /**
-   * Enqueue stylesheet.
-   * 
-   * @param string $handle
-   * @param string $src
-   * @param array|null $dependencies
-   * @param string|null $version
-   * @param string|null $media
-   * 
-   * @return void
-   * 
-   * @see https://developer.wordpress.org/reference/functions/wp_enqueue_style/
-   * @since 1.0.0
-   */
-  public static function enqueue_style(  
-    string $handle, 
-    string $src, 
-    array $dependencies = [], 
-    string $version = null, 
-    string $media = 'all' 
-  ):void {
-    $handle = static::$handle_prefix . '-' . $handle;
-
-    add_action( 
-      'wp_enqueue_scripts', 
-      function() use ( $handle, $src, $dependencies, $version, $media ) {
-        self::register_style( $handle, $src, $dependencies, $version, $media );
-		    wp_enqueue_style( $handle );
-	  } 
-    );
-  }
-
-  /**
-   * Enqueue script.
-   * 
-   * @param string $handle
-   * @param string $src
-   * @param array|null $dependencies
-   * @param string|null $version
-   * @param bool $in_footer
-   * 
-   * @return void
-   * 
-   * @see https://developer.wordpress.org/reference/functions/wp_enqueue_script/
-   * @since 1.0.0
-   */
-  public static function enqueue_script(  
-    string $handle, 
-    string $src, 
-    array|null $dependencies = [], 
-    string $version = null, 
-    bool $in_footer = true
-  ):void {
-    $handle = static::$handle_prefix . '-' . $handle;
-
-    add_action( 
-      'wp_enqueue_scripts', 
-      function() use ( $handle, $src, $dependencies, $version, $in_footer ) {
-        self::register_script( $handle, $src, $dependencies, $version, $in_footer );
-		    wp_enqueue_script( $handle );
-	  } 
-    );
-  }
-
-  /**
-   * Enqueue editor stylesheet.
-   * 
-   * @param string $handle
-   * @param string $src
-   * 
-   * @see https://developer.wordpress.org/reference/hooks/enqueue_block_editor_assets/
-   * @since 1.0.0
-   */
-  public static function enqueue_editor_style( string $handle, string $src ):void {
-    add_action( 'enqueue_block_editor_assets', function() use ( $handle, $src ) {
-      $handle = static::$handle_prefix . '-editor-' . $handle;
-
-      self::register_style( $handle, $src );
-      wp_enqueue_style( $handle );
-    } );
-  }
-
-  /**
-   * Enqueue editor script.
-   * 
-   * @param string $handle
-   * @param string $src
-   * 
-   * @see https://developer.wordpress.org/reference/hooks/enqueue_block_editor_assets/
-   * @since 1.0.0
-   */
-  public static function enqueue_editor_script( string $handle, string $src ):void {
-    add_action( 'enqueue_block_editor_assets', function() use ( $handle, $src ) {
-      $handle = static::$handle_prefix . '-editor-' . $handle;
-
-      self::register_script( $handle, $src );
-      wp_enqueue_script( $handle );
-    } );
-  }
-
-  /**
-   * Check for and return asset dependencies.
-   * 
-   * @param string $src
-   * @param array|null $dependencies
-   * 
-   * @return array
-   * 
-   * @since 1.0.0
-   */
-  private static function dependencies( string $src, array|null $dependencies ):array {
-    if (isset($dependencies) && is_array($dependencies) && isset($dependencies[0]) ) return $dependencies;
-
-    $script_asset = self::script_asset( $src );
-
-    return isset($script_asset['dependencies']) ? $script_asset['dependencies'] : [];
-  }
-
-  /**
-   * Check for and return asset version.
-   * 
-   * @param string $src
-   * @param string|null $version
-   * 
-   * @return string
-   * 
-   * @since 1.0.0
-   */
-  private static function version( string $src, string|null $version ): string {
-    if (isset($version) && is_string($version)) return $version;
-
-    $script_asset = self::script_asset( $src );
-
-    return isset($script_asset['version']) ? $script_asset['version'] : filemtime( $src );
-  }
-
-  /**
-   * Check for and return script asset.
-   * 
-   * Mainly useful for Gutenberg blocks and plugins.
-   * 
-   * @param string $src
-   * 
-   * @return array
-   * 
-   * @since 1.0.0
-   */
-  private static function script_asset( string $src ): array {
-    if ( pathinfo( $src, PATHINFO_EXTENSION ) !== 'js' ) return [];
-
-    $script_asset = pathinfo( $src, PATHINFO_DIRNAME ) . 'index.asset.php';
-
-    if ( file_exists($script_asset) ) {
-      $script_asset = require $script_asset;
-
-      return $script_asset;
-    } else {
-      return [];
     }
-  }
+
+    /**
+     * Enqueue stylesheet.
+     *
+     * @param string $handle
+     * @param string $src
+     * @param array|null $dependencies
+     * @param string|null $version
+     * @param string|null $media
+     *
+     * @return void
+     *
+     * @see https://developer.wordpress.org/reference/functions/wp_enqueue_style/
+     * @since 1.0.0
+     */
+    public static function enqueue_style(
+        string $handle,
+        string $src,
+        array $dependencies = [],
+        string $version = null,
+        string $media = 'all'
+    ):  void {
+        $handle = static::$handle_prefix . '-' . $handle;
+
+        add_action(
+            'wp_enqueue_scripts',
+            function() use ( $handle, $src, $dependencies, $version, $media ) {
+                self::register_style( $handle, $src, $dependencies, $version, $media );
+	        	wp_enqueue_style( $handle );
+	        }
+        );
+    }
+
+    /**
+     * Enqueue script.
+     *
+     * @param string $handle
+     * @param string $src
+     * @param array|null $dependencies
+     * @param string|null $version
+     * @param bool $in_footer
+     *
+     * @return void
+     *
+     * @see https://developer.wordpress.org/reference/functions/wp_enqueue_script/
+     * @since 1.0.0
+     */
+    public static function enqueue_script(
+        string $handle,
+        string $src,
+        array|null $dependencies = [],
+        string $version = null,
+        bool $in_footer = true
+    ): void {
+        $handle = static::$handle_prefix . '-' . $handle;
+
+        add_action(
+            'wp_enqueue_scripts',
+            function() use ( $handle, $src, $dependencies, $version, $in_footer ) {
+                self::register_script( $handle, $src, $dependencies, $version, $in_footer );
+	    	    wp_enqueue_script( $handle );
+	        }
+        );
+    }
+
+    /**
+     * Enqueue editor stylesheet.
+     *
+     * @param string $handle
+     * @param string $src
+     *
+     * @see https://developer.wordpress.org/reference/hooks/enqueue_block_editor_assets/
+     * @since 1.0.0
+     */
+    public static function enqueue_editor_style( string $handle, string $src ): void {
+        add_action(
+            'enqueue_block_editor_assets',
+            function() use ( $handle, $src ) {
+                $handle = static::$handle_prefix . '-editor-' . $handle;
+
+                self::register_style( $handle, $src );
+                wp_enqueue_style( $handle );
+            }
+        );
+    }
+
+    /**
+     * Enqueue editor script.
+     *
+     * @param string $handle
+     * @param string $src
+     *
+     * @see https://developer.wordpress.org/reference/hooks/enqueue_block_editor_assets/
+     * @since 1.0.0
+     */
+    public static function enqueue_editor_script( string $handle, string $src ): void {
+        add_action(
+            'enqueue_block_editor_assets',
+            function() use ( $handle, $src ) {
+                $handle = static::$handle_prefix . '-editor-' . $handle;
+
+                self::register_script( $handle, $src );
+                wp_enqueue_script( $handle );
+            }
+        );
+    }
+
+    /**
+     * Check for and return asset dependencies.
+     *
+     * @param string $src
+     * @param array|null $dependencies
+     *
+     * @return array
+     *
+     * @since 1.0.0
+     */
+    private static function dependencies( string $src, array|null $dependencies ): array {
+        if ( isset($dependencies) && is_array($dependencies) && isset($dependencies[0]) ) return $dependencies;
+
+        $script_asset = self::script_asset( $src );
+
+        return isset($script_asset['dependencies']) ? $script_asset['dependencies'] : [];
+    }
+
+    /**
+     * Check for and return asset version.
+     *
+     * @param string $src
+     * @param string|null $version
+     *
+     * @return string
+     *
+     * @since 1.0.0
+     */
+    private static function version( string $src, string|null $version ): string {
+        if (isset($version) && is_string($version)) return $version;
+
+        $script_asset = self::script_asset( $src );
+
+        return isset($script_asset['version']) ? $script_asset['version'] : filemtime( $src );
+    }
+
+    /**
+     * Check for and return script asset.
+     *
+     * Mainly useful for Gutenberg blocks and plugins.
+     *
+     * @param string $src
+     *
+     * @return array
+     *
+     * @since 1.0.0
+     */
+    private static function script_asset( string $src ): array {
+        if ( pathinfo( $src, PATHINFO_EXTENSION ) !== 'js' ) return [];
+
+        $script_asset = pathinfo( $src, PATHINFO_DIRNAME ) . 'index.asset.php';
+
+        if ( file_exists($script_asset) ) {
+          $script_asset = require $script_asset;
+
+          return $script_asset;
+        } else {
+          return [];
+        }
+    }
 }
